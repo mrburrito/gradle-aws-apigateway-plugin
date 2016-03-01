fe5077aecb94e1a7c8da8fb74ee150450744479d
Gordon Shankman <gshankman@gmail.com>
2016-03-01 13:26:19 -0500

    Modified CustomDomain to check for the existence of a base path mapping, updating it if found and creating if it it is missing. Fixes #12

--- src/main/groovy/com/shankyank/gradle/aws/apigateway/model/CustomDomain.groovy
+++ src/main/groovy/com/shankyank/gradle/aws/apigateway/model/CustomDomain.groovy
@@ -1,12 +1,17 @@
 package com.shankyank.gradle.aws.apigateway.model
 
 import com.amazonaws.services.apigateway.model.CreateBasePathMappingRequest
+import com.amazonaws.services.apigateway.model.GetBasePathMappingRequest
+import com.amazonaws.services.apigateway.model.PatchOperation
+import com.amazonaws.services.apigateway.model.UpdateBasePathMappingRequest
 import groovy.transform.Canonical
+import groovy.util.logging.Slf4j
 
 /**
  * A custom domain name registered in API Gateway.
  */
 @Canonical
+@Slf4j('logger')
 class CustomDomain implements ApiGatewayContainer {
     /** The custom domain name. */
     String domainName
@@ -18,6 +23,31 @@ class CustomDomain implements ApiGatewayContainer {
      * @param stageName the name of the target stage
      */
     void mapBasePathToStage(final String basePath, final String apiId, final String stageName) {
+        basePathMappingExists(basePath) ? updateExistingMapping(basePath, apiId, stageName) : createMapping(basePath, apiId, stageName)
+    }
+
+    private boolean basePathMappingExists(final String basePath) {
+        findNullableObject {
+            apiGateway.getBasePathMapping(new GetBasePathMappingRequest(
+                    domainName: domainName,
+                    basePath: basePath
+            ))
+        }
+    }
+
+    private void updateExistingMapping(final String basePath, final String apiId, final String stageName) {
+        List patchOps = [
+                new PatchOperation(op: 'replace', path: '/restapiId', value: apiId),
+                new PatchOperation(op: 'replace', path: '/stage', value: stageName)
+        ]
+        apiGateway.updateBasePathMapping(new UpdateBasePathMappingRequest(
+                domainName: domainName,
+                basePath: basePath,
+                patchOperations: patchOps
+        ))
+    }
+
+    private void createMapping(final String basePath, final String apiId, final String stageName) {
         apiGateway.createBasePathMapping(new CreateBasePathMappingRequest(
                 domainName: domainName,
                 basePath: basePath,
